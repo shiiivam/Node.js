0753a4bb47ceee84e65cf91b0ef7458b354c6f05
shiiivam <61604895+shiiivam@users.noreply.github.com>
2020-12-24 23:05:38 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,37 +1,41 @@
+const { Socket } = require('socket.io');
+
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+app.get("/",(req,res)=>{
+    res.sendfile("index.html");
+    //res.sendfile('index.html');
+});
+
+io.on('connection',(socket)=>{
+    console.log(" A user connected");
+    socket.on('disconnect',()=>{
+        console.log("A user disconnected");
+    });
+});
 
 
+http.listen(3000,console.log("Listening on port : 3000"));
 
+// var app = require('express')();
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
 
+// app.get('/', function(req, res) {
+//    res.sendfile('index.html');
+// });
 
+// //Whenever someone connects this gets executed
+// io.on('connection', function(socket) {
+//    console.log('A user connected');
 
+//    //Whenever someone disconnects this piece of code executed
+//    socket.on('disconnect', function () {
+//       console.log('A user disconnected');
+//    });
+// });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//2 . Importing first module from logger.js
-
-// const fun_log = require('./logger');
-// fun_log('shivam');
-
-
-
-
-// 1. First node progam
-
-// // function sayHello(name){
-// //  console.log("Hello "+name);
-// // }
-// // sayHello('mosh');
+// http.listen(3000, function() {
+//    console.log('listening on *:3000');
+// });
